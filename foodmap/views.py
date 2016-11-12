edd352d0d89d8a734af0035f3b7fcfaaaf6ff1c9
Phan Anh Nguyen <tonnpa@gmail.com>
2016-11-12 18:57:45 -0500

    feat(ui): map shows all restaurants
    Signed-off-by: Phan Anh Nguyen <tonnpa@gmail.com>

--- foodmap/views.py
+++ foodmap/views.py
@@ -1,8 +1,15 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from foodmap.models import Category
+from foodmap.models import Restaurant
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'HelloMap.html')
+    restaurants = Restaurant.objects.all()
+    categories = Category.objects.all()
+    context = {
+        'restaurant_list': restaurants,
+        'category_list': categories,
+    }
+    return render(request, 'HelloLeaflet.html', context)
